ef6b8213a2eedf3c31ab843f5b68bb5e5f796eba
Michael Chan <mikechan1234@gmail.com>
2023-06-25 16:11:16 +0100

    - Added remaining missing files

---
+++ NavigationExample/Views/ItemA/ItemAContainerView.swift
@@ -0,0 +1,30 @@
+//
+//  ItemAContainerView.swift
+//  NavigationExample
+//
+//  Created by Michael Chan on 25/06/2023.
+//
+
+import SwiftUI
+
+struct ItemAContainerView: View {
+    @StateObject var navigator: ItemANavigator
+    var body: some View {
+        NavigationStack(path: $navigator.path) {
+            navigator.rootView()
+            .navigationDestination(for: ItemARoute.self) { route in
+                navigator.view(for: route)
+            }
+        }.onOpenURL { url in
+            navigator.handle(url: url)
+        }
+    }
+}
+
+struct ItemAContainerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemAContainerView(
+            navigator: ItemANavigator()
+        )
+    }
+}
