4f3f45e3633694008c75546737908528a85c8e2e
Michael Chan <mikechan1234@gmail.com>
2023-06-25 13:09:48 +0100

    Initial Commit

---
+++ NavigationExample/NavigationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationExampleApp.swift
+//  NavigationExample
+//
+//  Created by Michael Chan on 25/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
